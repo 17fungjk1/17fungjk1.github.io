5707d528cb1723178d02387cdffffa8fc0779dbc
17fungjk1 <17fungjk1@stconline.edu.hk>
2015-03-18 14:56:11 +0800

    NEW
    NEW

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var MyNumber = 4;
+alert("My number is" + myNumber)
+MyNumber = 2;
+alert("actually, my number now is" + myNumber);
+var yourNumber = prompt("Give me a number!");
+alert("I have" + myNumber + "and you have" + yourNumber);
+alert("Together, we have " + (myNumber + yourNumber));
+alert("Oops, I mean we have " + (myNumber + +yourNumber));
